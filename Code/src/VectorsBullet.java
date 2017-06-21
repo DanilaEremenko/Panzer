4ae14e0b5f1e82ed66d720983790e6d0d33f0ec0
danil <danilaeremenko@mail.ru>
2017-06-21 14:46:33 +0300

    Заготовки

---
+++ Code/src/VectorsBullet.java
@@ -0,0 +1,36 @@
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
+
+import java.awt.geom.Point2D;
+
+public class VectorsBullet extends Pane {
+    public javafx.geometry.Point2D velocity = new javafx.geometry.Point2D(0, 0);
+
+
+    public VectorsBullet() {
+        getChildren().addAll(new Rectangle(20, 2, Color.RED));
+
+    }
+
+    public void setTarget(double x, double y) {
+        velocity = new javafx.geometry.Point2D(x, y).subtract(getTranslateX(), getTranslateY()).normalize().multiply(5);
+        double angle = calcAngle(velocity.getX(), velocity.getY());
+        getTransforms().clear();
+        getTransforms().add(new Rotate(angle,0,0));
+
+    }
+
+    public void move() {
+        setTranslateX(getTranslateX() + velocity.getX());
+        setTranslateY(getTranslateY() + velocity.getY());
+    }
+
+    public double calcAngle(double vecX, double vecY) {
+        double angle = new javafx.geometry.Point2D(vecX, vecY).angle(1, 0);
+        return vecY > 0 ? angle : -angle;
+    }
+
+}
+
