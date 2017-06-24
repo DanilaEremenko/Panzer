b0a6f583057bc8e27f195a248686b5d261858a28
danil <danilaeremenko@mail.ru>
2017-06-24 19:18:13 +0300

    Заготовки

--- Code/src/Snake.java
+++ Code/src/Snake.java
@@ -8,36 +8,81 @@ import java.awt.geom.RoundRectangle2D;
 public class Snake extends Pane {
     public javafx.geometry.Point2D velocity = new javafx.geometry.Point2D(0, 0);
     int width = 20;
+    String vector;
 
     public Snake() {
-        Rectangle rect=new Rectangle(width,20,Color.RED);
+        Rectangle rect = new Rectangle(width, 20, Color.RED);
         getChildren().addAll(rect);
     }
 
-    public void setTarget(double x, double y) {
-        velocity = new javafx.geometry.Point2D(x, y).subtract(getTranslateX(), getTranslateY()).normalize().multiply(5);
-        double angle = calcAngle(velocity.getX(), velocity.getY());
-        getTransforms().clear();
-        getTransforms().add(new Rotate(angle, 0, 0));
+    public Snake(Snake snake) {
+        Rectangle rect = new Rectangle(width, 20, Color.BLACK);
+        getChildren().addAll(rect);
+        setTranslateX(snake.getTranslateX() - 20);
+        setTranslateY(snake.getTranslateY());
 
     }
 
+    //Точка в которую движется голова
+    public void setTarget(double x, double y) {
+        velocity = new javafx.geometry.Point2D(x, y).subtract(getTranslateX(), getTranslateY()).normalize().multiply(5);
+        //double angle = calcAngle(velocity.getX(), velocity.getY());
+        //getTransforms().clear();
+        //getTransforms().add(new Rotate(angle, 0, 0));
+
+    }
+
+
+    public String getVector() {
+        return vector;
+
+    }
+
+
+    //Движение для головы
     public void move() {
         setTranslateX(getTranslateX() + velocity.getX());
         setTranslateY(getTranslateY() + velocity.getY());
 
     }
 
+
+    //Движение для хвотов
+    public void move(Snake snake) {
+        if (snake.getTranslateX() != getTranslateX() && snake.getTranslateY() != getTranslateY()) {
+            if (snake.getVector().equals("R") || snake.getVector().equals("L")) {
+                setTranslateY(snake.getTranslateY());
+                if (snake.getTranslateY() == getTranslateY() + 20)
+                    vector = "D";
+                else if (snake.getTranslateY() == getTranslateY() - 20)
+                    vector = "U";
+            } else if (snake.getVector().equals("U") || snake.getVector().equals("D")) {
+                setTranslateX(snake.getTranslateX());
+                if (snake.getTranslateX() == getTranslateX() + 20)
+                    vector = "R";
+                else if (snake.getTranslateY() == getTranslateY() - 20)
+                    vector = "L";
+            }
+        } else if (snake.getTranslateX() == getTranslateX() && snake.getTranslateY() < getTranslateY() + 20) {
+            setTranslateY(snake.getTranslateY() + 20);
+            vector = "U";
+        } else if (snake.getTranslateX() == getTranslateX() && snake.getTranslateY() > getTranslateY() - 20) {
+            setTranslateY(snake.getTranslateY() - 20);
+            vector = "D";
+        } else if (snake.getTranslateY() == getTranslateY() && snake.getTranslateX() > getTranslateX() + 20) {
+            setTranslateX(snake.getTranslateX() - 20);
+            vector = "R";
+        } else if (snake.getTranslateY() == getTranslateY() && snake.getTranslateX() < getTranslateX() - 20) {
+            setTranslateX(snake.getTranslateX() + 20);
+            vector = "L";
+        }
+
+    }
+
+
     public double calcAngle(double vecX, double vecY) {
         double angle = new javafx.geometry.Point2D(vecX, vecY).angle(1, 0);
         return vecY > 0 ? angle : -angle;
-    }
-
-    public void sizePlus() {
-        width += 20;
-        Rectangle rect=new Rectangle(width,20,Color.RED);
-        getChildren().addAll(rect);
-
     }
 
 
--- Code/src/SnakeGame.java
+++ Code/src/SnakeGame.java
@@ -6,6 +6,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 /**
  * Created by danil on 23.06.2017.
  */
@@ -13,21 +15,35 @@ public class SnakeGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Pane root = new Pane();
-        Snake snake=new Snake();
-        snake.setTranslateX(200);
-        snake.setTranslateY(200);
-        SnakeGift snakeGift=new SnakeGift();
-        root.getChildren().addAll(snake, snakeGift);
+        Snake head = new Snake();
+        head.setTranslateX(200);
+        head.setTranslateY(200);
+        SnakeGift snakeGift = new SnakeGift();
+        ArrayList<Snake> snakeTails = new ArrayList<Snake>();
+        snakeTails.add(new Snake(head));
+        root.getChildren().addAll(head, snakeTails.get(snakeTails.size() - 1), snakeGift);
         Scene scene = new Scene(root, 600, 600);
         scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.LEFT)
-                snake.setTarget(snake.getTranslateX()-10, snake.getTranslateY());
-            if(event.getCode()==KeyCode.RIGHT)
-                snake.setTarget(snake.getTranslateX()+10,snake.getTranslateY());
-            if(event.getCode()==KeyCode.UP)
-                snake.setTarget(snake.getTranslateX(),snake.getTranslateY()-10);
-            if(event.getCode()==KeyCode.DOWN)
-                snake.setTarget(snake.getTranslateX(),snake.getTranslateY()+10);
+            if (event.getCode() == KeyCode.LEFT) {
+                head.setTarget(head.getTranslateX() - 20, head.getTranslateY());
+                head.vector = "L";
+            }
+            if (event.getCode() == KeyCode.RIGHT) {
+                head.setTarget(head.getTranslateX() + 20, head.getTranslateY());
+                head.vector = "R";
+            }
+            if (event.getCode() == KeyCode.UP) {
+                head.setTarget(head.getTranslateX(), head.getTranslateY() - 20);
+                head.vector = "U";
+            }
+            if (event.getCode() == KeyCode.DOWN) {
+                head.setTarget(head.getTranslateX(), head.getTranslateY() + 20);
+                head.vector = "D";
+            }
+
+//            for (int i = 1; i < snakeTails.size(); i++)
+//                snakeTails.get(i).setTarget(snakeTails.get(i - 1));
+
         });
 
 
@@ -37,10 +53,17 @@ public class SnakeGame extends Application {
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                snake.move();
-                if (snake.getBoundsInParent().intersects(snakeGift.getBoundsInParent())) {
+
+                head.move();
+                snakeTails.get(0).move(head);
+                for (int i=1;i<snakeTails.size();i++)
+                    snakeTails.get(i).move(snakeTails.get(i-1));
+
+                if (head.getBoundsInParent().intersects(snakeGift.getBoundsInParent())) {
                     snakeGift.generateNew();
-                    snake.sizePlus();
+                    snakeTails.add(new Snake(snakeTails.get(snakeTails.size() - 1)));
+//                    snakeTails.get(snakeTails.size() - 1).move();
+                    root.getChildren().addAll(snakeTails.get(snakeTails.size() - 1));
                 }
 
             }
