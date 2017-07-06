08d33d95d84637eadf45f0333f49687f0d96e21c
DanilaEremenko <danilaeremenko@mail.ru>
2017-07-06 18:27:34 +0300

    Update PanzerElement.java

--- Code/src/Panzer/PanzerElement.java
+++ Code/src/Panzer/PanzerElement.java
@@ -4,10 +4,11 @@ import javafx.scene.shape.Rectangle;
 
 //Класс для создания препеятствий
 public class PanzerElement extends Pane {
-    static PanzerElement gorizontal=PanzerElement.generateGorizontal(PanzerGame.sceneWidt);
-    static PanzerElement gorizontal2=PanzerElement.generateGorizontal(PanzerGame.sceneWidt);
-    static PanzerElement vertical=PanzerElement.generateVertical(PanzerGame.sceneHeight);
-    static PanzerElement vertical2=PanzerElement.generateVertical(PanzerGame.sceneHeight);
+
+    static PanzerElement gorizontal=PanzerElement.generateGorizontal(PanzerGame.sceneWidt,0,0);
+    static PanzerElement gorizontal2=PanzerElement.generateGorizontal(PanzerGame.sceneWidt,0,PanzerGame.sceneHeight-20);
+    static PanzerElement vertical=PanzerElement.generateVertical(PanzerGame.sceneHeight,0,0);
+    static PanzerElement vertical2=PanzerElement.generateVertical(PanzerGame.sceneHeight,PanzerGame.sceneWidt-20,0);
 
     public static PanzerElement generateRectangle(int side) {
         Rectangle rect = new Rectangle(side, side, Color.BLACK);
@@ -16,17 +17,22 @@ public class PanzerElement extends Pane {
         return element;
     }
 
-    public static PanzerElement generateVertical(int height) {
+    public static PanzerElement generateVertical(int height,int X,int Y) {
         Rectangle rect = new Rectangle(20, height,Color.BLACK);
         PanzerElement element = new PanzerElement();
         element.getChildren().addAll(rect);
+        element.setTranslateX(X);
+        element.setTranslateY(Y);
         return element;
     }
 
-    public static PanzerElement generateGorizontal(int width) {
+    public static PanzerElement generateGorizontal(int width,int X,int Y) {
         Rectangle rect = new Rectangle(width, 20,Color.BLACK);
         PanzerElement element = new PanzerElement();
         element.getChildren().addAll(rect);
+        element.setTranslateX(X);
+        element.setTranslateY(Y);
         return element;
     }
+
 }
