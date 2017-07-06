24bf2b5be6eb424c0bf1af19d20eb85237d97449
DanilaEremenko <danilaeremenko@mail.ru>
2017-07-06 18:23:00 +0300

    Update Levels.java

--- Code/src/Panzer/Levels.java
+++ Code/src/Panzer/Levels.java
@@ -1,80 +1,103 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-
+import java.io.*;
 import java.util.ArrayList;
 
 
 //Класс для создания и хранения карт
 public class Levels {
 
-    enum LevelNumbers {ONE, TWO;}
 
-    static ArrayList<Panzer>panzers=new ArrayList<>();
+    //[0] Для колличества танков, вертикальных ограждений и горизонтальных ограждений
+    //[1] Для координат танков,сначала X и Y чередуются allDigits[0].get[0]-число танков
+    //[2] Для координат вертикальных препятствий X и Y чередуются allDigits[0].get[1]-число вертикальных препятствий
+    //[3] Для координат горизонтальных препятствий X и Y чередуются allDigits[0].get[2]-число горизонтальных препятствий
+    //[4] Для рамки либо 1 либо 0
+    private static ArrayList<Integer>[] allDigits = new ArrayList[5];
+    static ArrayList<Panzer> panzers = new ArrayList<>();
+    static ArrayList<PanzerElement> elements = new ArrayList<>();
+    static Scene scene;
+    static Scene menu;
 
-    static public void generate(String fileName){
+    public static void generateLevel(String fileName) throws IOException {
+        FileReader fileReader = new FileReader(new File(fileName));
+        for (int i = 0; i < allDigits.length; i++)
+            allDigits[i] = new ArrayList<Integer>();
+        int c;
+        int digit = 0;
+        int i = 0;//Идентификатор листа который заполняем
+        boolean pars = false;
+        while ((c = fileReader.read()) != -1) {
+            while (pars) {
+                if ((char) c == '>') {
+                    allDigits[i].add(digit);
+                    pars = false;
+                    digit = 0;
+                    break;
+                }
+                digit = digit * 10 + Character.getNumericValue(c);
+                c = fileReader.read();
+            }
+            if ((char) c == '<')
+                pars = true;
 
+            if ((char) c == '|')
+                i++;
 
-
-    }
-
-    public static void level(LevelNumbers digit) {
-        switch (digit) {
-            case ONE:
-                panzers.add(new Panzer(Color.GREEN));
-                panzers.add(new Panzer(Color.RED));
-                panzers.get(0).setTranslateX(100);
-                panzers.get(0).setTranslateY(200);
-                panzers.get(1).setTranslateX(500);
-                panzers.get(1).setTranslateY(600);
-                panzers.get(0).addOpponent(panzers.get(1));
-                panzers.get(1).addOpponent(panzers.get(0));
-
-                Panzer.elements.add(PanzerElement.generateVertical(100));
-                Panzer.elements.add(PanzerElement.generateGorizontal(100));
-                Panzer.elements.add(PanzerElement.generateVertical(100));
-                Panzer.elements.add(PanzerElement.generateGorizontal(100));
-                Panzer.elements.add(PanzerElement.generateVertical(100));
-                Panzer.elements.add(PanzerElement.generateGorizontal(100));
-                Panzer.elements.add(PanzerElement.generateVertical(100));
-                Panzer.elements.add(PanzerElement.generateGorizontal(100));
-                Panzer.elements.get(0).setTranslateX(250);//вертикальная
-                Panzer.elements.get(0).setTranslateY(130);
-                Panzer.elements.get(1).setTranslateX(150);//горизонтальная
-                Panzer.elements.get(1).setTranslateY(230);
-                Panzer.elements.get(2).setTranslateX(500);//вертикальная
-                Panzer.elements.get(2).setTranslateY(130);
-                Panzer.elements.get(3).setTranslateX(520);//горизонтальная
-                Panzer.elements.get(3).setTranslateY(230);
-                Panzer.elements.get(4).setTranslateX(500);//вертикальная
-                Panzer.elements.get(4).setTranslateY(390);
-                Panzer.elements.get(5).setTranslateX(520);//горизонтальная
-                Panzer.elements.get(5).setTranslateY(370);
-                Panzer.elements.get(6).setTranslateX(250);//вертикальная
-                Panzer.elements.get(6).setTranslateY(390);
-                Panzer.elements.get(7).setTranslateX(150);//горизонтальная
-                Panzer.elements.get(7).setTranslateY(370);
-                //Рамка
-                PanzerElement.gorizontal.setTranslateY(PanzerGame.sceneHeight - 20);
-                PanzerElement.vertical.setTranslateX(PanzerGame.sceneWidt - 20);
-                Panzer.elements.add(PanzerElement.vertical);
-                Panzer.elements.add(PanzerElement.vertical2);
-                Panzer.elements.add(PanzerElement.gorizontal);
-                Panzer.elements.add(PanzerElement.gorizontal2);
-                break;
-
-            case TWO:
-                panzers.add(new Panzer(Color.GREEN));
-                panzers.add(new Panzer(Color.RED));
-                panzers.get(0).setTranslateX(100);
-                panzers.get(0).setTranslateY(200);
-                panzers.get(1).setTranslateX(500);
-                panzers.get(1).setTranslateY(600);
-                panzers.get(0).addOpponent(panzers.get(1));
-                panzers.get(1).addOpponent(panzers.get(0));
-
-                break;
 
         }
 
 
+        int numberCoordinate = 0;
+        for (i = 0; i < allDigits[0].get(0); i++) {
+            panzers.add(new Panzer(Color.GREEN, allDigits[1].get(numberCoordinate), allDigits[1].get(numberCoordinate + 1)));
+            numberCoordinate += 2;
+
+        }
+        for (Panzer panzer : panzers) {
+            panzer.addOpponents(panzers);
+
+        }
+        numberCoordinate = 0;
+        for (i = 0; i < allDigits[0].get(1); i++) {
+            elements.add(PanzerElement.generateVertical(100, allDigits[2].get(numberCoordinate), allDigits[2].get(numberCoordinate + 1)));
+            numberCoordinate += 2;
+        }
+        numberCoordinate = 0;
+        for (i = 0; i < allDigits[0].get(2); i++) {
+            elements.add(PanzerElement.generateGorizontal(100, allDigits[3].get(numberCoordinate), allDigits[3].get(numberCoordinate + 1)));
+            numberCoordinate += 2;
+        }
+
+        if (allDigits[4].get(0) == 1) {
+            elements.add(PanzerElement.gorizontal);
+            elements.add(PanzerElement.gorizontal2);
+            elements.add(PanzerElement.vertical);
+            elements.add(PanzerElement.vertical2);
+        }
+
+
+
+        Pane root = new Pane();
+
+        for (i = 0; i < Panzer.bulletDigit; i++)
+            for (Panzer panzer : Levels.panzers)
+                root.getChildren().addAll(panzer.getBullets().get(i));
+
+        for (PanzerElement element : Levels.elements)
+            root.getChildren().addAll(element);
+
+        for (Panzer panzer : Levels.panzers)
+            root.getChildren().addAll(panzer);
+
+
+        scene = new Scene(root, PanzerGame.sceneWidt, PanzerGame.sceneHeight);
+        menu = new Scene(PanzerMenu.menuBox, PanzerGame.sceneWidt, PanzerGame.sceneHeight);
+
+
+
     }
+
+
 }
